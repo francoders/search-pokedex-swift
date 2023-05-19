efe8d68b6d6af791e0084e574be11ad3e2890ffc
franciscocorvalan <francisco22corvalan@gmail.com>
2023-05-18 23:03:49 -0400

    Initial Commit

---
+++ search-pokedex-swift/search_pokedex_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  search_pokedex_swiftApp.swift
+//  search-pokedex-swift
+//
+//  Created by Francisco Corvalán on 18-05-23.
+//
+
+import SwiftUI
+
+@main
+struct search_pokedex_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
